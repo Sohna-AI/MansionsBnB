482a35cf0b0e1fdc973dd8f64bfc4b3bcdc7aebb
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2024-03-12 15:45:25 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const userRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
+
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', userRouter);
 
 router.post('/test', (req, res) => {
   res.json({
