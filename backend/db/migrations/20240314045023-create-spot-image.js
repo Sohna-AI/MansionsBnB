42e5310d3582bbe9c9afc0eab8d0c0fa29eef6f6
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2024-03-15 10:23:50 -0700

    added options for defining schema

--- backend/db/migrations/20240314045023-create-spot-image.js
+++ backend/db/migrations/20240314045023-create-spot-image.js
@@ -1,38 +1,47 @@
 'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+options.tableName = 'spotImages';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('spotImages', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      'spotImages',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        url: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        preview: {
+          type: Sequelize.BOOLEAN,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        },
       },
-      url: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      spotId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      preview: {
-        type: Sequelize.BOOLEAN,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('spotImages');
+    await queryInterface.dropTable(options);
   },
 };
