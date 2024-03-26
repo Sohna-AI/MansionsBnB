84c4062c8ccdceccf15c5e4e1515551f7419dfcb
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2024-03-25 20:53:34 -0700

    corrected demo reviews

--- backend/db/seeders/20240315022055-demo-review.js
+++ backend/db/seeders/20240315022055-demo-review.js
@@ -21,19 +21,19 @@ module.exports = {
 
     const demo_reviews = [
       {
-        userId: 1,
+        userId: 2,
         spotId: 1,
         review: 'Spot is amazing!',
         stars: 5,
       },
       {
-        userId: 2,
+        userId: 1,
         spotId: 2,
         review: 'Great spot with alot to do',
         stars: 4,
       },
       {
-        userId: 3,
+        userId: 2,
         spotId: 3,
         review: 'Loved the idea and how clean it was',
         stars: 3,
