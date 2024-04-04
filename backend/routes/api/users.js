5a179d88f8bc941457b92999fde89e6bde86f5e5
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2024-04-03 21:19:20 -0700

    created new error for user signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,6 +8,35 @@ const router = express.Router();
 
 router.post('/', validateSignup, async (req, res, next) => {
   const { firstName, lastName, email, username, password } = req.body;
+  const existingUserEmail = await User.unscoped().findOne({
+    where: {
+      email: email,
+    },
+  });
+  const existingUserName = await User.unscoped().findOne({
+    where: {
+      username: username,
+    },
+  });
+  if (existingUserEmail) {
+    const error = new Error('Signup failed');
+    error.status = 500;
+    error.title = 'User already exists';
+    error.errors = {
+      email: 'User with that email already exists',
+    };
+    return res.json(error);
+  }
+
+  if (existingUserName) {
+    const error = new Error('Signup failed');
+    error.status = 500;
+    error.title = 'User already exists';
+    error.errors = {
+      email: 'User with that username already exists',
+    };
+    return res.json(error);
+  }
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({
     firstName,
@@ -16,21 +45,6 @@ router.post('/', validateSignup, async (req, res, next) => {
     username,
     hashedPassword,
   });
-
-  const existingUserEmail = await User.unscoped().findOne({
-    where: {
-      email: email,
-    },
-  });
-
-  if (existingUserEmail) {
-    const error = new Error('Signup failed');
-    error.status = 500;
-    error.title = 'User already exists';
-    error.errors = {
-      email: 'User with that email already exists',
-    };
-  }
 
   const safeUser = {
     id: user.id,
