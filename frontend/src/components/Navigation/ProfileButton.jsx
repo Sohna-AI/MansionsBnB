ac98faefe097769a1ac66f03a3c8a33c91f5c92d
Pushpinder Singh <142683189+Sohna-AI@users.noreply.github.com>
2024-05-04 09:04:59 -0700

    finished profileButton

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,25 +1,45 @@
 import { FaUserCircle } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './ProfileButton.css';
 
 const ProfileButton = ({ user }) => {
   const dispatch = useDispatch();
+  const ulRef = useRef();
   const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = (e) => {
+    e.stopPropagation();
+    setShowMenu(!showMenu);
+  };
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener('click', closeMenu);
+  }, [showMenu]);
+
   const ulClassName = 'profile-dropdown' + (showMenu ? '' : ' hidden');
   return (
     <>
-      <button className="profile-button" onClick={() => setShowMenu(!showMenu)}>
+      <button className="profile-button" onClick={toggleMenu}>
         <FaUserCircle />
       </button>
-      <ul className={ulClassName}>
+      <ul className={ulClassName} ref={ulRef}>
         <li>{user.username}</li>
         <li>
           {user.firstName} {user.lastName}
